87b05eb84af283279663759ba6ad2112e97a49fc
Konstantin Sychyov <ksychyov@alvioneurope.ru>
2019-09-04 15:24:58 +0300

    c

--- wp-content/themes/ace-corporate/js/functions.js
+++ wp-content/themes/ace-corporate/js/functions.js
@@ -245,3 +245,8 @@ function footerBarHeight(){
     jQuery('head').append('<style>.footer-widget .foot-bor:before{height:'+footcontentHeight+'px;}</style>');
 }
 
+jQuery("body").on('click', '[href*="#"]', function(e){
+    var fixed_offset = 100;
+    jQuery('html,body').stop().animate({ scrollTop: jQuery(this.hash).offset().top - fixed_offset }, 1000);
+    e.preventDefault();
+});
